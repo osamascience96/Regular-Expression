9056f4a618a6c8ac3160556b85def1144cb936f8
osamascience96 <osamahu96@gmail.com>
2020-04-26 13:01:19 +0500

    Experiment 4

--- experiment_day_4/experiment3.py
+++ experiment_day_4/experiment3.py
@@ -3,13 +3,26 @@
 
 import re
 
-s1 = "aabbbbabababababa"
+s1 = "abbbabababababa"
 s2 = "abbababababababab"
 
 
 # a followed by only 3 b's or more occurances of b and then followed by any number of character
 regex = "ab{3}.*"
 
-print(re.search(regex, s2))
+print(re.match(regex, s1))
 
-# to be continued
+s3 = "abbbbaaaabababaaaabbba"
+
+# a followed by 3 minimum or more than 3 occurances of b and then followed by any number of characters
+regex = "ab{3,}.*"
+
+print(re.match(regex, s3))
+
+s4 = "abbbbbactreihewrha"
+
+# a followed by 2 minimum or 5 maximum occurances of b and then followed by any number of characters but not b
+regex = "^ab{2,5}[^b]+$"
+
+print(re.match(regex, s4))
+
